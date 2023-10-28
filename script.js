a9bffc309966a65cf03f8c50e7e97c98be3880c7
nihatran <89045243+nihatran@users.noreply.github.com>
2023-10-28 16:37:03 -0400

    Add player's choice

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ function getComputerChoice() {
     let computerChoiceInt = Math.floor(Math.random() * 3) + 1;
     
     if (computerChoiceInt === 1) {
-        return "Rock";
+        return "rock";
     } else if (computerChoiceInt === 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
+// get player's choice
+let playerChoice = prompt("Rock, Paper, or Scissors?");
+playerChoice = playerChoice.toLowerCase();
+
+// get computer's choice
 let computerChoice = getComputerChoice();
+
+
